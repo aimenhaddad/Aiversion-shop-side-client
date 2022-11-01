d98c6716d698f304115acf59327eaf4f56142f1b
Dell <aymen.had2001@gmail.com>
2022-11-01 22:27:43 +0100

    aa

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Skycode-dz Shop</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="css/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/bootstrap.min.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="Product-table.php">
+                    <img src="assets/logo.svg" width="100" height="30" class="d-inline-block align-top" alt="">
+                </a>
+                <div>
+                    <form class="form-inline my-2 my-lg-0 ">
+                        <div>
+                            <span>  Skycode-dz Shop</span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">Skycode-dz Shop</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">The best from the best.</p>
+                </div>
+            </div>
+        </header>
+        <!-- Section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row justify-content-center">
+                    <div class="col-sm-12 col-md-4 col-xl-3 mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Fancy Product</h5>
+                                    <!-- Product price-->
+                                    $40.00 - $80.00
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="product_detail_page.php">View options</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Skycode-dz 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ product_detail_page.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Skycode-dz Shop</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="css/bootstrap-icons.css" rel="stylesheet" />
+        <link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet"/>
+
+        <!-- Core theme CSS (includes Bootstrap)-->
+         <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+          <!-- Navigation-->
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container px-4 px-lg-5">
+                    <a class="navbar-brand" href="index.php">
+                        <img src="assets/logo.svg" width="100" height="30" class="d-inline-block align-top" alt="">
+                    </a>
+                    <div>
+                        <form class="form-inline my-2 my-lg-0 ">
+                            <div>
+                                <span>  Skycode-dz Shop</span>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        <!-- Page header with logo and tagline-->
+ 
+        <!-- Page content-->
+        <div class="container">
+            <div class="row">
+                <!-- Blog entries-->
+                <div class="col-lg-8">
+                    <!-- Featured blog post-->
+                    <div class="card mb-4">
+                        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                        <div class="card-body">
+                            <div class="small text-muted"></div>
+                            <h2 class="card-title">Featured Post Title</h2>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+                        
+                        </div>
+                    </div>
+                   
+                </div>
+                <!-- Side widgets-->
+                <div class="col-lg-4">
+                    <!-- Search widget-->
+                    <div class="card mb-4">
+                        <div class="card-header">Order</div>
+                        <div class="card-body">
+                            <form class="well form-horizontal">
+                                   <div class="form-group mb-2">
+                                    <label class="col-md-4 control-label">Full Name</label>
+                                      <div class=" inputGroupContainer">
+                                         <div class="input-group-prepend"><input id="fullName" name="fullName" placeholder="Full Name" class="form-control" required="true" value="" type="text"></div>
+                                      </div>
+                                   </div>
+                                   <div class="form-group mb-2">
+                                    <label class="col-md-4 control-label">Phone</label>
+                                      <div class=" inputGroupContainer">
+                                         <div class="input-group-prepend"><input id="phone" name="phone" placeholder="Phone" class="form-control" required="true" value="" type="text"></div>
+                                      </div>
+                                   </div>
+                                   <div class="form-group mb-3">
+                                    <label class="control-label">Quantity</label>
+                                      <div class=" inputGroupContainer">
+                                         <div class="input-group"><input id="quantity" name="quantity" placeholder="Quantity" class="form-control" required="true" value="" type="number"></div>
+                                      </div>
+                                   </div>
+                                   <div class="form-group mb-3 ">
+                                      <div class="input-group ">
+                                         <select class="custom-select col-12" id="inputGroupSelect01">
+                                             <option selected>the state</option>
+                                             <option value="1">One</option>
+                                             <option value="2">Two</option>
+                                             <option value="3">Three</option>
+                                        </select>
+                                      </div>
+                                   </div>
+                                   
+                                   <div class="text-center">
+                                    <button  class="btn btn-outline-primary  col-12 " type="submit">Order Now!</button>
+                                   </div>
+                                  
+                            </form>  
+                        </div>
+                    </div>
+                    <!-- Categories widget-->
+                   
+                    <!-- Side widget-->
+                  
+                </div>
+            </div>
+        </div>
+        
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
